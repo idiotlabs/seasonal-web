b80d13fd384fef765cc8648b46f57eb0f6f02d20
Seobs <subi88@naver.com>
2023-05-07 16:18:05 +0900

    feat: add api for check version

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { State } from './entity/state.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(State) private stateRepository: Repository<State>,
+  ) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getVersion() {
+    const state = await this.stateRepository
+      .createQueryBuilder('state')
+      .where('seasonal_state = :state', { state: 'api_version' })
+      .getOne();
+
+    return { data: state.seasonal_value };
   }
 }
